073e0cd6683d19a4dc2c5d3883eb0f8b216b3cd7
zacacollier <zacacollier@gmail.com>
2016-11-09 23:27:53 -0600

    Wiring up the search bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
 import List from './components/List.js'
 import SearchBar from './components/SearchBar.js'
-class App extends Component {
+
+export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            searchText: '',
+            members: [
+                {
+                    _id: 'jerome',
+                    name: 'Jerry Seinfeld',
+                    occupation: 'Comedian',
+                    avatar: 'https://pbs.twimg.com/profile_images/433319671430123521/LAm8cB1b.jpeg'
+                },
+                {
+                    _id: 'elaine',
+                    name: 'Elaine Benes',
+                    occupation: 'Personal Assistant',
+                    avatar: 'http://vignette2.wikia.nocookie.net/parody/images/5/57/Elaine.jpg/revision/latest?cb=20140720224240'
+                },
+                {
+                    _id: 'george',
+                    name: 'George Costanza',
+                    occupation: 'Architect',
+                    avatar: 'http://www.seinfeld-fan.net/pictures/george/george_costanza029.jpg'
+                },
+                {
+                    _id: 'kramer',
+                    name: 'Cosmo Kramer',
+                    occupation: 'occupation unknown',
+                    avatar: 'http://marcmyers.typepad.com/.a/6a00e008dca1f0883401a511539249970c-600wi' 
+                },
+            ]
+ }
+        handleChange(event) {
+            this.setState({
+                members: this.state.members,
+                searchText: event.target.value
+            })
+        }
+
+        getFilteredList() {
+            const term = this.state.searchText.trim().toLowerCase()
+            const members = this.state.members
+
+            if (!term) return members
+
+            return members.filter(member => {
+                return member.name.toLowerCase().search(term) >= 0
+            })
+        }
+    }
   render() {
     return (
       <div className="App">
-            <SearchBar />
-            <List />
+            <SearchBar
+                value={this.state.searchText}
+        onChange={this.handleChange.bind(this)}
+            />
+            <List members={this.getFilteredList()} />
       </div>
     );
   }
 }
 
-export default App;
