4029409861b3f51e1de88db6408559433a7b9c19
zacacollier <zacacollier@gmail.com>
2016-11-29 20:21:22 -0600

    merged PropTypes into Master

--- src/App.js
+++ src/App.js
@@ -12,32 +12,6 @@ export default class App extends Component {
             searchText: '',
             members: []
         }
-        /* members: [
-         *       {
-         *           _id: 'jerome',
-         *           name: 'Jerry Seinfeld',
-         *           occupation: 'Comedian',
-         *           avatar: 'https://pbs.twimg.com/profile_images/433319671430123521/LAm8cB1b.jpeg'
-         *       },
-         *       {
-         *           _id: 'elaine',
-         *           name: 'Elaine Benes',
-         *           occupation: 'Personal Assistant',
-         *           avatar: 'http://vignette2.wikia.nocookie.net/parody/images/5/57/Elaine.jpg/revision/latest?cb=20140720224240'
-         *       },
-         *       {
-         *           _id: 'george',
-         *           name: 'George Costanza',
-         *           occupation: 'Architect',
-         *           avatar: 'http://www.seinfeld-fan.net/pictures/george/george_costanza029.jpg'
-         *       },
-         *       {
-         *           _id: 'kramer',
-         *           name: 'Cosmo Kramer',
-         *           occupation: 'occupation unknown',
-         *           avatar: 'http://marcmyers.typepad.com/.a/6a00e008dca1f0883401a511539249970c-600wi'
-         *       }
-         *   ]*/
  }
     handleChange(event) {
         this.setState({
